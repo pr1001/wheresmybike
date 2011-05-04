3ea90b5c4aacf47dab4d0d49c32c9e0d9ac66012
Peter Robinett <peter@bubblefoundry.com>
2011-05-04 21:55:04 +0200

    IT WORKS!

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
     var msg = isNew ? 'Successfully created!' : "Saved!";      
     // get all the inputs into an array.
     var values = {};
-    $('form input', self.el).each(function(i, field) {
+    $('form input, form select', self.el).each(function(i, field) {
       values[field.name] = field.value;
     });
     self.model.save(values, {
@@ -30,7 +30,15 @@ $(document).ready(function() {
           self.model = model;
           self.render();
           self.delegateEvents();
-          // return to home view
+          // set the right heart and buttons
+          if (WMB.theIncident) {
+            WMB.showBrokenHeart();
+          } else {
+            WMB.showHeart();
+          }
+          // set the right bike
+          $('#home_bike').attr('src', 'assets/bike_' + WMB.theBike.get('color') + '.png');
+          // go to home view
           WMB.showView(WMB.theViews['home']);
         },
         error: function() {
@@ -41,6 +49,9 @@ $(document).ready(function() {
   }
   
   WMB.colorsArr = ['black', 'blue', 'green', 'orange', 'red', 'silver', 'white', 'yellow'];
+  // set the current color in the picker to the saved bike color or default to black
+  var pos = WMB.colorsArr.indexOf(WMB.theBike.get('color'))
+  WMB.currentColor = pos > -1 ? pos : 0;
   WMB.colorDOMs = WMB.colorsArr.map(function (color) {
     var img = document.createElement('img');
     img.src = 'assets/bike_' + color + '.png';
@@ -48,80 +59,140 @@ $(document).ready(function() {
     $(img).addClass('bike');
     return img;
   });
-/*
-  console.log("before ll")
-  WMB.colors = new CircularTwoWayLinkedList(colorDOMs);
-  console.log("after ll")
-  // advance to list to the appropriate color (default is black)
-  var toColor = WMB.theBike.get('color') || 'black';
-  while (WMB.colors.current().value.alt != toColor) {
-    WMB.colors.next();
-  }
-*/
   
   // custom render
   WMB.theBike.saveView.render = function() {
-    // var out = '<form>';
-    // out += "<label for='title'>Title</label>";
-    // out += "<input name='title' type='text' />";
-    // out += "<label for='body'>Body</label>";
-    // out += "<textarea name='body'>" + (this.model.escape('body') || '') + "</textarea>";
-    // var submitText = this.model.isNew() ? 'Create' : 'Save';
-    // out += "<button>" + submitText + "</button>";
-    // out += "</form>";
-    
     var form = document.createElement('form');
     form.setAttribute('onsubmit', 'return false;');
     var model = this.model;
+    
+    // prep the main bike container
+    var infoContainer = document.createElement('div');
+    infoContainer.id = 'infoContainer';
+    var infoTitle = document.createElement('h2');
+    infoTitle.innerHTML = 'Register my bike';
+    infoContainer.appendChild(infoTitle);
+    var infoList = document.createElement('ul');
+    $(infoList).addClass('listBubble');
+    
+    // prep the engraving container
+    var engravingContainer = document.createElement('div');
+    engravingContainer.id = 'engravingContainer';
+    var engravingTitle = document.createElement('h2');
+    
+    engravingTitle.appendChild(document.createTextNode('Engraving '));
+    var q = document.createElement('a');
+    q.setAttribute('src', '#');
+    q.addEventListener('click', function (event) {
+      WMB.showView(WMB.theViews['engraving']);
+    });
+    q.innerHTML = '(?)';
+    engravingTitle.appendChild(q);
+    
+    engravingContainer.appendChild(engravingTitle);
+    var engravingList = document.createElement('ul');
+    $(engravingList).addClass('listBubble');
+    
     _.each(_.keys(model.attributes), function(attr) {
-      if (attr && attr != 'color' && attr != 'id' && attr != 'cid') {
+      // ignore frame, tag, and insurance numbers for now so that it fits into our form
+      if (attr == 'manufacturer' || attr == 'model' /* || attr == 'frame' || attr == 'tag' || attr == 'insurance' */) {
+        var li = document.createElement('li');
+        
         var label = document.createElement('label');
         label.setAttribute('for', attr);
-        label.appendChild(document.createTextNode(WMB.capitalize(attr)));
-        form.appendChild(label);
+        label.innerHTML = WMB.capitalize(attr);
+        li.appendChild(label);
+        
         var input = document.createElement('input');
         input.setAttribute('name', attr);
         input.setAttribute('type', 'text');
         input.setAttribute('value', model.get(attr));
-        form.appendChild(input);
-        if (attr == 'engraving') {
-          var q = document.createElement('img');
-          q.src = 'assets/question.png';
-          q.width = 20;
-          q.height = 20;
-          $(q).bind('click', function(event) {
-            WMB.showView(WMB.theViews['engraving']);
-          });
-          form.appendChild(q);
+        li.appendChild(input);
+        
+        infoList.appendChild(li);
+      }
+      else if (attr == 'year') {
+        var years = _.range(1970, (new Date().getFullYear() + 2)).reverse();
+        
+        var li = document.createElement('li');
+        
+        var label = document.createElement('label');
+        label.setAttribute('for', attr);
+        label.innerHTML = WMB.capitalize(attr);
+        li.appendChild(label);
+        
+        var input = document.createElement('select');
+        input.setAttribute('name', attr);
+        var bikesYear = WMB.theBike.get('year');
+        for (var k = 0; k < years.length; k++) {
+          var year = years[k];
+          var option = document.createElement('option');
+          option.setAttribute('value', year);
+          option.innerHTML = year;
+          if (year == bikesYear) {
+            // input.selectedIndex = k;
+            option.setAttribute('selected', 'selected');
+          }
+          input.appendChild(option);
         }
-      } else if (attr == 'color') {
+        // years.forEach(function (year) {});
+        li.appendChild(input);
+        
+        infoList.appendChild(li);
+      }
+      // else if color
+      else if (attr == 'color') {
+        var li = document.createElement('li');
+        
         var colorContainer = document.createElement('div');
         colorContainer.id = 'bike_color_container';
         colorContainer.setAttribute('for', attr);
         var colorDiv = document.createElement('div');
         colorDiv.id = 'bike_color_picker';
-        // var img = document.createElement('img');
-        // img.src = 'apple-touch-icon.png';
-        // img.alt = '';
-        // colorDiv.appendChild(img);
-        // FIXME: Have it track current position so we don't need to increment like this
         for (var k = 0; k < WMB.colorDOMs.length; k++) {
           colorDiv.appendChild(WMB.colorDOMs[k]);
         }
         colorContainer.appendChild(colorDiv); // document.createTextNode(WMB.capitalize(attr))
-        form.appendChild(colorContainer);
+        li.appendChild(colorContainer);
         var input = document.createElement('input');
+        input.id = 'bike_color_input';
         input.setAttribute('name', attr);
         input.setAttribute('type', 'hidden');
         input.setAttribute('value', model.get(attr));
-        form.appendChild(input);
+        li.appendChild(input);
+        
+        infoList.appendChild(li);
       }
-      if (attr && attr != 'id' && attr != 'cid') {
-        form.appendChild(document.createElement('br'));
+      // else if engraving
+      else if (attr == 'engraving') {
+        var li = document.createElement('li');
+        
+        var label = document.createElement('label');
+        label.setAttribute('for', attr);
+        label.innerHTML = WMB.capitalize(attr);
+        li.appendChild(label);
+        
+        var input = document.createElement('input');
+        input.setAttribute('name', attr);
+        input.setAttribute('type', 'text');
+        input.setAttribute('value', model.get(attr));
+        li.appendChild(input);
+        
+        engravingList.appendChild(li);
       }
     });
+    
+    // now add the info container
+    infoContainer.appendChild(infoList);
+    form.appendChild(infoContainer);
+    
+    // now add the engraving container
+    engravingContainer.appendChild(engravingList);
+    form.appendChild(engravingContainer);
+    
     var submit = document.createElement('input');
     submit.setAttribute('type', 'submit');
+    submit.setAttribute('value', 'Save');
     // make sure our new save function gets called, not the old one
     submit.addEventListener('click', WMB.theBike.saveView.save);
     form.appendChild(submit);
@@ -139,65 +210,76 @@ $(document).ready(function() {
   
   WMB.theBike.saveView.render();
   
+  WMB.showBikeColor = function showBikeColor(k) {
+    if (k == 0) {
+      $('#bike_color_picker').css('left', '60px');
+    } else {
+      $('#bike_color_picker').css('left',  (k * -168 + 60) + 'px');
+    }
+  }
+  
+  WMB.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
   WMB.engravings = [
     {
-      'start': new Date('2011-05-04 10:30'),
-      'end': new Date('2011-05-04 15:30'),
+      // subtract 1 from the normal months because the Date's months are zero-indexed
+      'start': new Date(2011,05-1,04,10,30),
+      'end': new Date(2011,05-1,04,15,30),
       'place': 'Koningsplein',
       'longitude': 4.889492690563202,
       'latitude': 52.36736862768112
     }, {
-      'start': new Date('2011-05-11 10:30'),
-      'start': new Date('2011-05-11 15:30'),
-      'place': 'Museumplein, naast de AH ingang',
+      'start': new Date(2011,05-1,11,10,30),
+      'start': new Date(2011,05-1,11,15,30),
+      'place': 'Museumplein',
       'longitude': 4.8798950761556625,
       'latitude': 52.35706886536736
     }, {
-      'start': new Date('2011-05-18 10:30'),
-      'end': new Date('2011-05-18 15:30'),
-      'place': 'Osdorpplein, naast de Halfords',
+      'start': new Date(2011,05-1,18,10,30),
+      'end': new Date(2011,05-1,18,15,30),
+      'place': 'Osdorpplein',
       'longitude': 4.806998670101166,
       'latitude': 52.35969921225086
     }, {
-      'start': new Date('2011-05-25 10:30'),
-      'end': new Date('2011-05-25 15:30'),
-      'place': 'Gulden Winckelplantsoen, naast KFC',
+      'start': new Date(2011,05-1,25,10,30),
+      'end': new Date(2011,05-1,25,15,30),
+      'place': 'Gulden Winckelplantsoen',
       'longitude': 4.847386032342911,
       'latitude': 52.3784523267057
     }, {
-      'start': new Date('2011-06-01 10:30'),
-      'end': new Date('2011-06-01 15:30'),
-      'place': 'Zwanenburgwal, t.o. Dantzig',
+      'start': new Date(2011,06-1,01,10,30),
+      'end': new Date(2011,06-1,01,15,30),
+      'place': 'Zwanenburgwal',
       'longitude': 4.8989541828632355,
       'latitude': 52.36763148527381
     }, {
-      'start': new Date('2011-06-08 10:30'),
-      'end': new Date('2011-06-08 15:30'),
-      'place': 'Javaplein, naast het Badhuis',
+      'start': new Date(2011,06-1,08,10,30),
+      'end': new Date(2011,06-1,08,15,30),
+      'place': 'Javaplein',
       'longitude': 4.9398645758628845,
       'latitude': 52.36369992233441
     }, {
-      'start': new Date('2011-06-15 10:30'),
-      'end': new Date('2011-06-15 15:30'),
+      'start': new Date(2011,06-1,15,10,30),
+      'end': new Date(2011,06-1,15,15,30),
       'place': 'Heinekenplein',
       'longitude': 4.891131520271301,
       'latitude': 52.357052893609584
     }, {
-      'start': new Date('2011-06-22 10:30'),
-      'end': new Date('2011-06-22 15:30'),
-      'place': 'Haarlemmerstraat, op de Eenhoornsluis',
+      'start': new Date(2011,06-1,22,10,30),
+      'end': new Date(2011,06-1,22,15,30),
+      'place': 'Haarlemmerstraat',
       'longitude': 4.88929957151413,
       'latitude': 52.38142644728091
     }, {
-      'start': new Date('2011-06-29 10:30'),
-      'end': new Date('2011-06-29 15:30'),
-      'place': 'Museumplein, naast de ingang van AH',
+      'start': new Date(2011,06-1,29,10,30),
+      'end': new Date(2011,06-1,29,15,30),
+      'place': 'Museumplein',
       'longitude': 4.8798950761556625,
       'latitude': 52.35706886536736
     }, {
-      'start': new Date('2011-07-06 10:30'),
-      'end': new Date('2011-07-06 15:30'),
-      'place': 'Osdorpplein, naast de Halfords',
+      'start': new Date(2011,07-1,06,10,30),
+      'end': new Date(2011,07-1,06,15,30),
+      'place': 'Osdorpplein',
       'longitude': 4.806998670101166,
       'latitude': 52.35969921225086
     }
@@ -215,25 +297,6 @@ $(document).ready(function() {
     $('#home_bike_container').css('background-image', 'url(assets/heart.png)');
     $('#home_buttons_bad').hide();
     $('#home_buttons_good').show();
-  }
-  
-  WMB.showBikeColor = function showBikeColor(color) {
-    console.log(WMB.colorsArr);
-    for (var k = 0; k < WMB.colorsArr.length; k++) {
-      if (WMB.colorsArr[k] == color) {
-        if (k == 0) {
-          console.log('chose the first color')
-          $(WMB.colorDOMs[k]).css('left', '0px');
-        } else {
-          console.log('setting to -' + (k * 168) + 'px');
-          $('#bike_color_picker').css('left',  (k * -168 + 30) + 'px');
-        }
-        break;
-      } else {
-        console.log(WMB.colorsArr[k] + " isn't " + color)
-      }
-    }
-    return true;
   }
   
   // check AFAC
@@ -265,21 +328,22 @@ $(document).ready(function() {
     
     $.ajax({
       type: 'POST', // defaults to 'GET'
-      url: 'http://www.afac-amsterdam.nl/bikebase/page.php?category=item', // defaults to window.location
+      // url: 'http://www.afac-amsterdam.nl/bikebase/page.php?category=item', // defaults to window.location
+      url: 'http://labs.bubblefoundry.com/wheresmybike/ajax_proxy.php',
       data: data, // can be a string or object (objects are automatically serialized to JSON)
-      dataType: 'html', // what response type you accept from the server ('json', 'xml', 'html', or 'text')
-      success: function(html) {
+      dataType: 'json', // what response type you accept from the server ('json', 'xml', 'html', or 'text')
+      success: function(successObj) {
         // not found
-        if (!html || html.indexOf('De fiets gezocht met deze gegevens is niet gevonden in de database.') > -1) {
+        if (!successObj || !successObj.success) {
           console.log("AFAC doesn't know about the bike.")
-          $('#find_afac_indicator').html("No");
+          $('#find_afac_indicator').html('<img src="assets/triangle.png" alt="Bike not found">');
           // $('#find_afac_indicator').html("&#x2717");
           // $('body').get(0).appendChild(document.createTextNode("AFAC doesn't know about the bike."))
         }
         // FOUND!
         else {
           console.log("AFAC has the bike!");
-          $('#find_afac_indicator').html("Yes");
+          $('#find_afac_indicator').html('<img src="assets/bike_found.png" alt="Bike found!">');
           // $('#find_afac_indicator').html("&#x2714");
           // $('body').get(0).appendChild(document.createTextNode("AFAC has the bike!"))
         }
@@ -293,6 +357,35 @@ $(document).ready(function() {
   
   // WMB.theBike.saveView.el.style.display = "none";
   document.getElementById("register_view").appendChild(WMB.theBike.saveView.el);
+  
+  // add listeners to the color picker
+  WMB.picker = $('#bike_color_picker');
+  WMB.bike_color_input = $('#bike_color_input');
+  // throttle since we seem to get two at a time
+  WMB.lastSwipeLeft = new Date;
+  WMB.lastSwipeRight = new Date;
+  WMB.swipeLeftFunc = function () {
+    var now = new Date;
+    // if the last swipe was more than 10 ms ago and it's not the end
+    if (now - WMB.lastSwipeLeft > 10 && WMB.currentColor < WMB.colorsArr.length - 1) {
+      WMB.currentColor += 1;
+      WMB.bike_color_input.attr('value', WMB.colorsArr[WMB.currentColor]);
+      WMB.showBikeColor(WMB.currentColor);
+    }
+    WMB.lastSwipeLeft = now;
+  };
+  WMB.picker.swipeLeft(WMB.swipeLeftFunc);
+  WMB.swipeRightFunc = function () {
+    var now = new Date;
+    // if the last swipe was more than 10 ms ago and it's not the beginning
+    if (now - WMB.lastSwipeRight > 10 && WMB.currentColor > 0) {
+      WMB.currentColor -= 1;
+      WMB.bike_color_input.attr('value', WMB.colorsArr[WMB.currentColor]);
+      WMB.showBikeColor(WMB.currentColor);
+    }
+    WMB.lastSwipeRight = now;
+  };
+  WMB.picker.swipeRight(WMB.swipeRightFunc);
   
   WMB.previousView = null;
   // grab the DOM elements once
@@ -311,6 +404,20 @@ $(document).ready(function() {
   WMB.backButton = $('#back_button');
   // the magic happens here
   WMB.showView = function showView(view) {
+    // since we're switching views, there's so reason to show the Add to Home image anymore
+    $('footer img').hide();
+    
+    // make sure we're defaulting the color picker to the right color
+    if (view.attr('id') == 'register_view') {
+      var color = WMB.theBike.get('color');
+      WMB.currentColor = WMB.colorsArr.indexOf(color);
+      WMB.bike_color_input.attr('value', color);
+      WMB.showBikeColor(WMB.currentColor);
+      // add the listeners again because they seem to get lost somehow? Doesn't seem to work.
+      // WMB.picker.swipeLeft(WMB.swipeLeftFunc);
+      // WMB.picker.swipeRight(WMB.swipeRightFunc);
+    }
+    
     // hide previous view
     if (WMB.previousView) {
       WMB.previousView.toggleClass('selected');
@@ -327,6 +434,8 @@ $(document).ready(function() {
       WMB.backButton.bind('click', function() {
         // save the bike
         WMB.theBike.saveView.save();
+        // update home screen bike
+        $('#home_bike').attr('src', 'assets/bike_' + WMB.theBike.get('color') + '.png');
         WMB.showView(WMB.theViews['home']);
       });
     }
@@ -349,11 +458,9 @@ $(document).ready(function() {
       // Work around iOS bug (yes, in-place filtering is a bug)
       var copy = WMB.engravings
       copy.filter(function (time) {
-        console.log('filter');
         return time.start >= now;
       });
       copy.sort(function (time1, time2) {
-        console.log('sort');
         if (time1 > time2) {
           return 1;
         }
@@ -363,11 +470,25 @@ $(document).ready(function() {
         return 0;
       });
       copy.forEach(function (time) {
-        console.log('forEach');
         var item = document.createElement('li');
+        
+        var dateSpan = document.createElement('span');
+        $(dateSpan).addClass('date');
+        dateSpan.innerHTML = WMB.months[time.start.getMonth()] + ' ' + time.start.getDate();
+        item.appendChild(dateSpan);
+        
+        var name = document.createElement('span');
+        $(name).addClass('name');
+        name.innerHTML = time.place;
+        item.appendChild(name);
+        
         var a = document.createElement('a');
         a.href = 'http://maps.google.com/?ll=' + time.latitude + ',' + time.longitude + '&q=' + time.latitude + ',' + time.longitude;
-        a.appendChild(document.createTextNode(time.place));
+        var mapButton = document.createElement('button');
+        $(mapButton).addClass('map');
+        mapButton.innerHTML = 'Map';
+        a.appendChild(mapButton);
+        
         item.appendChild(a);
         list.append(item);
       });
@@ -381,12 +502,14 @@ $(document).ready(function() {
     }
     // else if on find view 
     else if (view.attr('id').indexOf('find') === 0) {
+      // check AFAC
+      WMB.theBike.checkAFAC();
+      
       if (!WMB.theIncident) {
         // create new Incident
         WMB.theIncident = new WMB.Incident;
         WMB.theIncident.setBike(WMB.theBike);
         // NB setBike also saves the model so don't have to do it ourselves
-        // use broken heart
         // broken heart
         WMB.showBrokenHeart();
       }
